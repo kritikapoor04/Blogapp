83a315ed276ef697ba526fe8b39e5d20c4c1d321
kritikapoor04 <kritikapoor2401@gmail.com>
2022-05-28 00:40:03 +0530

    added

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,16 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption=models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.caption
+
 class Author(models.Model):
     first_name= models.CharField(max_length=50)
     last_name= models.CharField(max_length=50)
     email= models.EmailField()
+
+    def __str__(self):
+        return self.first_name+" "+self.last_name
 class Post(models.Model):
     title = models.CharField(max_length=50)
     excerpt = models.CharField(max_length=150)
